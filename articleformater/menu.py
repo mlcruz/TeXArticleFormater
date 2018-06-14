2c5c5ca1e8f6192032eeae4628debd6c0baab166
mlcruz <mlcruz@inf.ufrgs.br>
2018-06-14 13:47:17 -0300

    Linux terminal version
    Added command line interface

--- articleformater/menu.py
+++ articleformater/menu.py
@@ -1,103 +1,45 @@
-import TeXanalyser
+import click
 from TeXanalyser import Article
-import colorama
-import easygui
-import os
+@click.command()
+@click.option('--tex_path',prompt='Path to tex file', help='Path to tex file')
+@click.option('--bib_path',prompt='Path to bib file', help='Path to bib file')
+@click.option('--tex_output_name',default="default",help='Output tex file path. Defaults to file.tex.new in the same folder')
+@click.option('--bib_output_name',default="default",help='Output bib file path. Defaults to file.bib.new in the same folder')
+@click.option('--remove_uncited',default="n",help='y/n Removes unused bibliography entries. Default: n')
+@click.option('--log_file_path',default="default",help='Output .log file path. Defaults to file.log in the same folder')
+@click.option('--comment_removed',default="n",help='y/n Comments removed fields from bib data. default: n')
+@click.option('--terminator',default="N/A",help="String to fill missing entries with. Defaults to N/A")
 
+def console_wrapper(bib_path,tex_path,tex_output_name,bib_output_name,remove_uncited,log_file_path,comment_removed,terminator):
 
-colorama.init()
+    dados = Article(tex_path,bib_path)
 
-
-used_options = {"file_input":"dialog","uncited":"no","save_name":"default(file_new.tex)","terminator":"N/A","log_name":"default(file.log)","comment_removed":"no"}
-unused_options ={"file_input":"console", "uncited":"yes","save_name":"open save dialog","log_name":"open save dialog","comment_removed":"yes"}
-restart = '0'
-
-print(colorama.Fore.CYAN + colorama.Style.BRIGHT + "Options:\n")
-print(colorama.Fore.GREEN + "1)File input: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["file_input"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["file_input"]))
-print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "2)Remove uncited bibliography entries: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["uncited"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["uncited"]))
-print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "3)New file name: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["save_name"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["save_name"]))
-print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "4)Missing entry text: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["terminator"]))
-print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "5)Log file name: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["log_name"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["log_name"]))
-print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "6)Comment removed fields: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["comment_removed"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["comment_removed"]))
-
-print(colorama.Style.BRIGHT + colorama.Fore.CYAN + "\nEnter a number to change options or 0 to continue" + colorama.Style.RESET_ALL)
-
-
-#i wish python had do-whiles..
-user_input = input()
-while (restart == '0'):
-    while (user_input != "0"):
-        if user_input == "1":
-            (used_options["file_input"],unused_options["file_input"]) = (unused_options["file_input"],used_options["file_input"])
-        elif user_input == "2":
-            (used_options["uncited"],unused_options["uncited"]) = (unused_options["uncited"],used_options["uncited"])
-        elif user_input == "3":
-            (used_options["save_name"],unused_options["save_name"]) = (unused_options["save_name"],used_options["save_name"])
-        elif user_input == "4":
-            used_options["terminator"] = input("Enter new text: ")
-        elif user_input == "5":
-            (used_options["log_name"],unused_options["log_name"]) = (unused_options["log_name"],used_options["log_name"])
-        elif user_input =="6":
-            (used_options["comment_removed"],unused_options["comment_removed"]) = (unused_options["comment_removed"],used_options["comment_removed"])
-
-
-
-
-
-
-
-        os.system('cls')
-        print(colorama.Fore.CYAN + colorama.Style.BRIGHT + "Options:\n")
-        print(colorama.Fore.GREEN + "1)file input: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["file_input"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["file_input"]))
-        print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "2)Remove uncited bibliography entries: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["uncited"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["uncited"]))
-        print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "3)New file name: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["save_name"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["save_name"]))
-        print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "4)Missing entry text: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["terminator"]))
-        print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "5)Log file name: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["log_name"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["log_name"]))
-        print(colorama.Style.BRIGHT + colorama.Fore.GREEN + "6)Comment removed fields: " + colorama.Fore.LIGHTYELLOW_EX + "{0}".format(used_options["comment_removed"]) + " / " + colorama.Style.DIM +colorama.Fore.LIGHTBLACK_EX + "{0}".format(unused_options["comment_removed"]))
-
-
-        print(colorama.Style.BRIGHT + colorama.Fore.CYAN + "\nEnter a number to change options or 0 to continue" + colorama.Style.RESET_ALL)
-
-
-        user_input = input()
-
-
-    if used_options["file_input"] == "dialog":
-
-        print("Select Tex and Bib files")
-        dados = Article(easygui.fileopenbox("Select tex File",None,"*.tex"),easygui.fileopenbox("Select viv File",None,"*.bib"))
-    else:
-        dados = Article(input("tex file location: "),input("bib file location: "))
-
-    if used_options["uncited"] == "yes":
+    if remove_uncited == ("y" or "yes" or "YES" or "Y"):
         dados.bib_data.cite_block_library = dados.bib_data.cull_useless(dados.tex_data.cited_list)
 
-    if used_options["comment_removed"] == "yes":
-        dados.current_bib_data = dados.bib_data.generate_writable_bib_object(used_options["terminator"],1)
+    if comment_removed == ("y" or "yes" or "YES" or "Y"):
+        dados.current_bib_data = dados.bib_data.generate_writable_bib_object(terminator,1)
     else:
-        dados.current_bib_data = dados.bib_data.generate_writable_bib_object(used_options["terminator"],0)
+        dados.current_bib_data = dados.bib_data.generate_writable_bib_object(terminator,0)
 
-    if used_options["save_name"] == "default(file_new.tex)":
-        dados.write_bib()
+    if tex_output_name == "default":
         dados.write_tex()
+
     else:
-        dados.tex_file_location = easygui.filesavebox("new tex file name",None,(("{0}_{1}").format(((dados.tex_file_location).split(".")[0]).lower(),"new.tex")),r'*.tex')
-
-        dados.bib_file_location = easygui.filesavebox("new bib file name",None,(("{0}_{1}").format(((dados.bib_file_location).split(".")[0]).lower(),"new.bib")),r'*.bib')
-
-        dados.write_bib(1)
+        dados.tex_file_location = tex_output_name
         dados.write_tex(1)
 
-    print(colorama.Fore.RED + colorama.Style.BRIGHT + "Completed!\n")
-    
-    if used_options["log_name"] == "default(file.log)":
+    if bib_output_name == "default":
+        dados.write_bib()
+    else:
+        dados.bib_file_location = bib_output_name
+        dados.write_bib(1)
+
+    if log_file_path == "default":
         dados.write_log()
     else:
-        dados.log_file_location = easygui.filesavebox("New log file name",None,dados.log_file_location,r"*.log")
+        dados.log_file_location = log_file_path
         dados.write_log(1)
 
-  
-    
-    print(colorama.Fore.CYAN + "press 0 to restart\n")
-    restart = input()
-    user_input = ''
+if __name__ == '__main__':
+    console_wrapper()
